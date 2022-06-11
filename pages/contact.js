ee9711531b468201bd54d52aab71429436900e7f
unsivil <unsivilaudio@gmail.com>
2022-06-11 10:40:32 -0600

    couple of fixes

--- pages/contact.js
+++ pages/contact.js
@@ -6,7 +6,7 @@ const ContactPage = props => {
     return (
         <div className={classes.ContactPage}>
             <Head>
-                <title>Contact Me</title>
+                <title>Contact Me | Bloggr</title>
                 <meta name='description' content='Get in touch with me.' />
             </Head>
             <div className={classes.Header}>Get in touch with us!</div>
--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,12 +1,14 @@
-import AllPosts from 'components/posts/AllPosts';
+import Head from 'next/head';
+
 import { getAllPosts } from 'lib/posts-util';
+import AllPosts from 'components/posts/AllPosts';
 import classes from 'styles/pages/AllPostsPage.module.scss';
 
 const AllPostsPage = props => {
     return (
         <div className={classes.AllPostsPage}>
             <Head>
-                <title>All Posts</title>
+                <title>All Posts | Bloggr</title>
                 <meta
                     name='description'
                     content='A list of all programming-related tutorials and posts.'
